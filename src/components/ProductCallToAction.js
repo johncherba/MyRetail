90970d772226228d69e5429b8f421df78ca9d726
John Cherba <john.cherba@mchsi.com>
2019-03-15 22:48:59 -0500

    Update ProductCallToAction.js

--- src/components/ProductCallToAction.js
+++ src/components/ProductCallToAction.js
@@ -3,14 +3,14 @@ import {connect} from "react-redux";
 import "./ProductCallToAction.css";
 import loader from "../images/loader.gif";
 
-class ProductCallToAction extends React.Component {
-    constructor(props) {
-        super(props);
+class AvailableInStore extends React.Component {
+    constructor() {
+        super();
 
-        this.state = {};
+        this.state = {
+            message: 'click me'
+        };
         this.pickUpInStore = this.pickUpInStore.bind(this);
-        this.addToCart = this.addToCart.bind(this);
-        this.findInAStore = this.findInAStore.bind(this);
     }
 
     pickUpInStore() {
@@ -19,16 +19,42 @@ class ProductCallToAction extends React.Component {
         });
     }
 
+    render() {
+        return <button onClick={this.pickUpInStore} className="black">pick up in store</button>
+    }
+}
+
+class AvailableOnline extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            message: 'click me'
+        };
+        this.addToCart = this.addToCart.bind(this);
+    }
+
     addToCart() {
         this.setState({
             message: "added to cart"
         });
     }
 
-    findInAStore() {
-        this.setState({
-            message: "find in a store"
-        });
+    render() {
+        return <button onClick={this.addToCart} className="red">add to cart</button>
+    }
+}
+
+class ProductCallToAction extends React.Component {
+    static availableInStore(products) {
+        let pCC = '';
+        products.map(product => (
+                pCC = Number(product.purchasingChannelCode)
+            )
+        );
+        if (pCC === 0 || pCC === 2) {
+            return <AvailableInStore PurchasingChannelCode={pCC}/>
+        }
     }
 
     static availableOnline(products) {
@@ -39,20 +65,9 @@ class ProductCallToAction extends React.Component {
         );
         if (pCC === 0 || pCC === 1) {
             console.log('purchasingChannelCode: ' + pCC + ' is typeof ' + (typeof (pCC) + ' and is available to test by changing item-data.json and restarting the json api server.js.'));
-            return <button onClick={this.addToCart} className="red">add to cart</button>
+            return <AvailableOnline PurchasingChannelCode={pCC}/>
         }
 
-    }
-
-    static availableInStore(products) {
-        let pCC = '';
-        products.map(product => (
-                pCC = Number(product.purchasingChannelCode)
-            )
-        );
-        if (pCC === 0 || pCC === 2) {
-            return <button onClick={this.pickUpInStore} className="black">pick up in store</button>
-        }
     }
 
     render() {
@@ -72,7 +87,6 @@ class ProductCallToAction extends React.Component {
             <div className={"ProductCallToAction"}>
                 {ProductCallToAction.availableInStore(products)}
                 {ProductCallToAction.availableOnline(products)}
-                {/*<sub>{this.state.message}</sub>*/}
             </div>
         );
     }
@@ -81,7 +95,7 @@ class ProductCallToAction extends React.Component {
 const mapStateToProps = state => ({
     products: state.products.items,
     loading: state.products.loading,
-    error: state.products.error
+    error: state.products.error,
 });
 
 export default connect(mapStateToProps)(ProductCallToAction);
